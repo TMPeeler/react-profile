b8046c2d9de9313b9e05c935246e723608807e93
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-25 16:52:23 -0700

    generate new HTML and add some folders for components

---
+++ tmpreactprofile/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home"
+import Cookbook from "./pages/Cookbook"
+import Pantry from "./pages/Pantry"
+import notFound from "./pages/notFound"
+import Navbar from "./components/Navbar"
+// import Searchbar from "./components/Searchbar"
+// import RecipeHeader from "./components/RecipeHeader"
+// import ChecklistDropdown from "./components/ChecklistDropdown"
+
+function App() {
+    return (
+        <div>
+
+
+
+
+            
+        </div>
+
+
+
+    )
+
+}
+
+export default App;
